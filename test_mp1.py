aa750452632c1b92ae63e6f0db2c442a943b9772
Alison Major <alison.major@gmail.com>
2021-09-12 13:32:45 -0400

    Create test for is_goal()

--- test_mp1.py
+++ test_mp1.py
@@ -1,12 +1,19 @@
 """ The unit tests for the PuzzleState class. """
 import unittest
+from parameterized import parameterized
 import numpy.testing
+import numpy as np
 
 from mp1 import PuzzleState
 
 
 class TestPuzzleState(unittest.TestCase):
     """ Will run tests against modules and functions in the PuzzleState class. """
+
+    def setUp(self):
+        active_state = np.loadtxt('./samples/mp1input1.txt', dtype=np.int32)
+        self._puzzle_state = PuzzleState(active_state, 0, None)
+
     def test_solved_puzzle_value(self):
         # pylint: disable=no-self-use
         """ Tests is the SOLVED_PUZZLE value matches the desired state. """
@@ -18,18 +25,31 @@ class TestPuzzleState(unittest.TestCase):
 
     def test_compute_heuristic_cost(self):
         """ TODO """
+        self.skipTest('Test not yet created')
 
-    def test_is_goal(self):
-        """ TODO """
+    @parameterized.expand([
+        ("should be exact match", [[0, 1, 2], [3, 4, 5], [6, 7, 8]], True),
+        ("should not match", [[8, 6, 7], [2, 0, 1], [3, 4, 5]], False)
+    ])
+    def test_is_goal(self, name, puzzle, expected):
+        # pylint: disable=unused-argument
+        """ Tests that we return True for goal match and False for not matched. """
+        self._puzzle_state.puzzle = puzzle
+        actual = self._puzzle_state.is_goal()
+        self.assertEqual(expected, actual)
 
     def test_show_path(self):
         """ TODO """
+        self.skipTest('Test not yet created')
 
     def test_can_move(self):
         """ TODO """
+        self.skipTest('Test not yet created')
 
     def test_gen_next_state(self):
         """ TODO """
+        self.skipTest('Test not yet created')
+
 
 if __name__ == '__main__':
     unittest.main()
