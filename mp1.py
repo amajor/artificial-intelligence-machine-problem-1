5eb41dacacb7d51d4064487cd6752412d020b947
Alison <alison.major@gmail.com>
2021-09-13 23:09:47 -0400

    Develop (#1)
    * Create pylintrc file
    
    ```shell
    pylint --generate-rcfile > ./.pylintrc
    ```
    
    * Improve pylint control with pylintrc file
    
    * Create test for is_goal()
    
    * Test the can_move() function
    
    * Test and build gen_next_state()
    
    * Improving comment grammar.
    
    * More renaming and cleanup
    
    * Remove print lines
    
    * Calculate and test the heuristic distance
    
    Use the Manhattan distance heuristic (the sum of the distances of the tiles from their goal positions).
    
    * Remove deprecated use of numpy.str
    
    * Add starting test for printed output
    
    * Move show_path() to end of file
    
    * Improve test comparison
    
    * Store action from each predecessor
    
    * Test show_path()
    
    * Add notes to print number of states vs. number of moves

--- mp1.py
+++ mp1.py
@@ -7,6 +7,7 @@ Machine Problem 1
 """
 
 import queue
+from copy import deepcopy
 import numpy as np
 
 
@@ -14,69 +15,121 @@ class PuzzleState:
     """This program implements A* for solving a sliding tile puzzle"""
     SOLVED_PUZZLE = np.arange(9).reshape((3, 3))
 
-    def __init__(self, conf, g, pred_state):
+    def __init__(self, conf, g, predecessor_state):
         self.puzzle = conf  # Configuration of the state
-        self.gcost = g  # Path cost
+        self.g_cost = g  # Path cost
         self._compute_heuristic_cost()  # Set heuristic cost
-        self.fcost = self.gcost + self.hcost
-        self.pred = pred_state  # Predecessor state
-        self.zeroloc = np.argwhere(self.puzzle == 0)[0]
-        self.action_from_pred = None
+        self.f_cost = self.g_cost + self.heuristic_cost
+        self.predecessor = predecessor_state  # Predecessor state
+        self.zero_location = np.argwhere(self.puzzle == 0)[0]
+        self.action_from_predecessor = None
 
     def __hash__(self):
         return tuple(self.puzzle.ravel()).__hash__()
 
     def _compute_heuristic_cost(self):
-        """ TODO: Actually calculate this! """
-        # """ Updates the heuristic function value for use in A* """
-        self.hcost = 5
+        """ Updates the heuristic function value for use in A*
+            using the Manhattan Distance Heuristic. """
+        heuristic_value = 0
+        tile_value = 0
+        while tile_value < 9:
+            # Determine current position of this number tile.
+            current_position = np.argwhere(self.puzzle == tile_value)[0]
+
+            # Determine desired position of this number tile.
+            desired_position = np.argwhere(self.SOLVED_PUZZLE == tile_value)[0]
+
+            # Calculate how many rows and columns the tile is from its desired position.
+            row_distance = abs(current_position[0] - desired_position[0])
+            col_distance = abs(current_position[1] - desired_position[1])
+
+            # Calculate heuristic value for this tile and add to total.
+            heuristic_value = heuristic_value + row_distance + col_distance
+
+            # Move to next numeric tile.
+            tile_value += 1
+
+        # Set the current heuristic value.
+        self.heuristic_cost = heuristic_value
 
     def is_goal(self):
-        """ Checks to see if current state matches the goal state. """
+        """ Checks to see if current state puzzle matches the goal state puzzle. """
         return np.array_equal(PuzzleState.SOLVED_PUZZLE, self.puzzle)
 
     def __eq__(self, other):
         return np.array_equal(self.puzzle, other.puzzle)
 
     def __lt__(self, other):
-        return self.fcost < other.fcost
+        return self.f_cost < other.f_cost
 
     def __str__(self):
-        return np.str(self.puzzle)
+        return str(self.puzzle)
 
     move = 0
 
     def show_path(self):
         """ Shows the path by printing each chosen state that leads to the goal. """
-        if self.pred is not None:
-            self.pred.show_path()
+        if self.predecessor is not None:
+            self.predecessor.show_path()
 
         if PuzzleState.move == 0:
             print('START')
         else:
-            print('Move', PuzzleState.move, 'ACTION:', self.action_from_pred)
+            print('Move', PuzzleState.move, 'ACTION:', self.action_from_predecessor)
         PuzzleState.move = PuzzleState.move + 1
         print(self)
 
     def can_move(self, direction):
-        # pylint: disable=no-self-use
-        """ TODO """
-        if direction == 'up':
-            print('Can I move up?')
+        """ Determines if the blank tile can move in a particular direction. """
+        row = self.zero_location[0]
+        col = self.zero_location[1]
 
-        if direction == 'down':
-            print('Can I move down?')
-
-        if direction == 'left':
-            print('Can I move left?')
-
-        if direction == 'right':
-            print('Can I move right?')
-
+        if direction == 'up' and row != 0:
+            return True
+        if direction == 'down' and row != 2:
+            return True
+        if direction == 'left' and col != 0:
+            return True
+        if direction == 'right' and col != 2:
+            return True
         return False
 
     def gen_next_state(self, direction):
-        """ TODO """
+        """ Generates the next state for the puzzle after moving blank in specified direction. """
+        # Find the current zero-location (blank space).
+        zero_row = self.zero_location[0]
+        zero_col = self.zero_location[1]
+
+        # Store the zero location values for our swap tile calculations.
+        swap_row = zero_row
+        swap_col = zero_col
+
+        # Find the value in the appropriate direction.
+        if direction == 'up':
+            swap_row -= 1
+        if direction == 'down':
+            swap_row += 1
+        if direction == 'left':
+            swap_col -= 1
+        if direction == 'right':
+            swap_col += 1
+
+        # Move the zero-location in the direction specified,
+        # swapping with the number in the location it moves to.
+        new_puzzle = deepcopy(self.puzzle)
+        new_puzzle[zero_row, zero_col], new_puzzle[swap_row, swap_col] = (
+            new_puzzle[swap_row, swap_col], new_puzzle[zero_row, zero_col]
+        )
+
+        # Create the new state.
+        path_cost = self.g_cost + 1
+        predecessor_state = self
+        next_state = PuzzleState(new_puzzle, path_cost, predecessor_state)
+
+        # Set the predecessor's direction being moved.
+        next_state.action_from_predecessor = direction
+
+        return next_state
 
 
 def main():
@@ -89,8 +142,8 @@ def main():
 
     # load random start state onto frontier priority queue
     frontier = queue.PriorityQueue()
-    active_state = np.loadtxt('./samples/mp1input1.txt', dtype=np.int32)
-    start_state = PuzzleState(active_state, 0, None)
+    puzzle_array = np.loadtxt('./samples/mp1input1.txt', dtype=np.int32)
+    start_state = PuzzleState(puzzle_array, 0, None)
 
     frontier.put(start_state)
 
@@ -98,31 +151,39 @@ def main():
 
     num_states = 0
     while not frontier.empty():
-        #  choose state at front of priority queue
+        #  Choose state at front of priority queue.
         next_state = frontier.get()
         num_states = num_states + 1
 
-        #  if goal then quit and return path
+        #  If puzzle matches goal, then quit and return path.
         if next_state.is_goal():
             next_state.show_path()
             break
 
-        # Add state chosen for expansion to closed_set
+        # Add state chosen for expansion to closed_set.
         closed_set.add(next_state)
 
-        # Expand state (up to 4 moves possible)
+        # Expand state (up to 4 moves possible).
         possible_moves = ['up', 'down', 'left', 'right']
         for move in possible_moves:
             if next_state.can_move(move):
                 neighbor = next_state.gen_next_state(move)
                 if neighbor in closed_set:
+                    # If it's already in the frontier,
+                    # it's guaranteed to have lower cost,
+                    # so no need to update.
                     continue
                 if neighbor not in frontier.queue:
                     frontier.put(neighbor)
-                # If it's already in the frontier, it's guaranteed to have lower cost,
-                # so no need to update.
 
-    print('\nNumber of states visited =', num_states)
+    # Number of states visited is inaccurate;
+    # more states were visited to calculate the moves.
+    # However, the example printout shows count for
+    # number of MOVES (not number of states).
+    print('\nNumber of states checked =', num_states)
+
+    # Printing number of moves to match printout example.
+    print('\nNumber of states visited =', PuzzleState.move)
 
 
 if __name__ == "__main__":
--- test_mp1.py
+++ test_mp1.py
@@ -1,35 +1,196 @@
 """ The unit tests for the PuzzleState class. """
+import io
+import sys
 import unittest
+
+from parameterized import parameterized
 import numpy.testing
+import numpy as np
 
 from mp1 import PuzzleState
 
 
 class TestPuzzleState(unittest.TestCase):
     """ Will run tests against modules and functions in the PuzzleState class. """
+
+    def setUp(self):
+        puzzle_array = np.loadtxt('./samples/mp1input1.txt', dtype=np.int32)
+        self._puzzle_state = PuzzleState(puzzle_array, 0, None)
+
     def test_solved_puzzle_value(self):
-        # pylint: disable=no-self-use
         """ Tests is the SOLVED_PUZZLE value matches the desired state. """
         desired = [[0, 1, 2],
                    [3, 4, 5],
                    [6, 7, 8]]
-        actual = PuzzleState.SOLVED_PUZZLE
+        actual = self._puzzle_state.SOLVED_PUZZLE
         numpy.testing.assert_allclose(desired, actual)
 
-    def test_compute_heuristic_cost(self):
-        """ TODO """
+    @parameterized.expand([
+        ("should have heuristic of 0", [[0, 1, 2], [3, 4, 5], [6, 7, 8]], 0),
+        ("should have heuristic of 2", [[1, 0, 2], [3, 4, 5], [6, 7, 8]], 2),
+        ("should have heuristic of 4", [[2, 0, 1], [3, 4, 5], [6, 7, 8]], 4),
+        ("should have heuristic of 6", [[3, 4, 5], [0, 1, 2], [6, 7, 8]], 6),
+        ("should have heuristic of 12", [[3, 4, 5], [6, 7, 8], [0, 1, 2]], 12),
+        ("should have heuristic of 14", [[3, 4, 5], [7, 6, 8], [0, 1, 2]], 14),
+        ("should have heuristic of 12", [[0, 4, 5], [7, 6, 8], [3, 1, 2]], 12)
+    ])
+    def test_compute_heuristic_cost(self, _test_name, puzzle, expected_heuristic_cost):
+        """ Tests the heuristic cost of the entire puzzle. """
+        test_state = np.asarray(puzzle, dtype=np.int32)
+        self._puzzle_state = PuzzleState(test_state, 0, None)
+        actual_heuristic_cost = self._puzzle_state.heuristic_cost
+        self.assertEqual(expected_heuristic_cost, actual_heuristic_cost)
 
-    def test_is_goal(self):
-        """ TODO """
+    @parameterized.expand([
+        ("should be exact match", [[0, 1, 2], [3, 4, 5], [6, 7, 8]], True),
+        ("should not match", [[8, 6, 7], [2, 0, 1], [3, 4, 5]], False)
+    ])
+    def test_is_goal(self, _test_name, puzzle, expected):
+        """ Tests that we return True for goal match and False for not matched. """
+        self._puzzle_state.puzzle = puzzle
+        actual = self._puzzle_state.is_goal()
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ("position [0 0] cannot move up", [[0, 1, 2], [3, 4, 5], [6, 7, 8]],  False),
+        ("position [0 1] cannot move up", [[1, 0, 2], [3, 4, 5], [6, 7, 8]], False),
+        ("position [0 2] cannot move up", [[1, 2, 0], [3, 4, 5], [6, 7, 8]], False),
+        ("position [1 0] can move up", [[1, 2, 3], [0, 4, 5], [6, 7, 8]], True),
+        ("position [1 1] can move up", [[1, 2, 3], [4, 0, 5], [6, 7, 8]], True),
+        ("position [1 2] can move up", [[1, 2, 3], [4, 5, 0], [6, 7, 8]], True),
+        ("position [2 0] can move up", [[1, 2, 3], [4, 5, 6], [0, 7, 8]], True),
+        ("position [2 1] can move up", [[1, 2, 3], [4, 5, 6], [7, 0, 8]], True),
+        ("position [2 2] can move up", [[1, 2, 3], [4, 5, 6], [7, 8, 0]], True)
+    ])
+    def test_can_move_up(self, _test_name, puzzle, expected):
+        """ Tests whether the blank space can move UP """
+        test_state = np.asarray(puzzle, dtype=np.int32)
+        self._puzzle_state = PuzzleState(test_state, 0, None)
+        actual = self._puzzle_state.can_move('up')
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ("position 1 can move down", [[0, 1, 2], [3, 4, 5], [6, 7, 8]], True),
+        ("position 2 can move down", [[1, 0, 2], [3, 4, 5], [6, 7, 8]], True),
+        ("position 3 can move down", [[1, 2, 0], [3, 4, 5], [6, 7, 8]], True),
+        ("position 4 can move down", [[1, 2, 3], [0, 4, 5], [6, 7, 8]], True),
+        ("position 5 can move down", [[1, 2, 3], [4, 0, 5], [6, 7, 8]], True),
+        ("position 6 can move down", [[1, 2, 3], [4, 5, 0], [6, 7, 8]], True),
+        ("position 7 cannot move down", [[1, 2, 3], [4, 5, 6], [0, 7, 8]], False),
+        ("position 8 cannot move down", [[1, 2, 3], [4, 5, 6], [7, 0, 8]], False),
+        ("position 9 cannot move down", [[1, 2, 3], [4, 5, 6], [7, 8, 0]], False)
+    ])
+    def test_can_move_down(self, _test_name, puzzle, expected):
+        """ Tests whether the blank space can move DOWN """
+        test_state = np.asarray(puzzle, dtype=np.int32)
+        self._puzzle_state = PuzzleState(test_state, 0, None)
+        actual = self._puzzle_state.can_move('down')
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ("position 1 cannot move left", [[0, 1, 2], [3, 4, 5], [6, 7, 8]], False),
+        ("position 2 can move left", [[1, 0, 2], [3, 4, 5], [6, 7, 8]],  True),
+        ("position 3 can move left", [[1, 2, 0], [3, 4, 5], [6, 7, 8]],  True),
+        ("position 4 cannot move left", [[1, 2, 3], [0, 4, 5], [6, 7, 8]], False),
+        ("position 5 can move left", [[1, 2, 3], [4, 0, 5], [6, 7, 8]], True),
+        ("position 6 can move left", [[1, 2, 3], [4, 5, 0], [6, 7, 8]], True),
+        ("position 7 cannot move left", [[1, 2, 3], [4, 5, 6], [0, 7, 8]], False),
+        ("position 8 can move left", [[1, 2, 3], [4, 5, 6], [7, 0, 8]], True),
+        ("position 9 can move left", [[1, 2, 3], [4, 5, 6], [7, 8, 0]], True)
+    ])
+    def test_can_move_left(self, _test_name, puzzle, expected):
+        """ Tests whether the blank space can move in LEFT """
+        test_state = np.asarray(puzzle, dtype=np.int32)
+        self._puzzle_state = PuzzleState(test_state, 0, None)
+        actual = self._puzzle_state.can_move('left')
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ("position 1 can move right", [[0, 1, 2], [3, 4, 5], [6, 7, 8]], True),
+        ("position 2 can move right", [[1, 0, 2], [3, 4, 5], [6, 7, 8]], True),
+        ("position 3 cannot move right", [[1, 2, 0], [3, 4, 5], [6, 7, 8]], False),
+        ("position 4 can move right", [[1, 2, 3], [0, 4, 5], [6, 7, 8]], True),
+        ("position 5 can move right", [[1, 2, 3], [4, 0, 5], [6, 7, 8]], True),
+        ("position 6 cannot move right", [[1, 2, 3], [4, 5, 0], [6, 7, 8]], False),
+        ("position 7 can move right", [[1, 2, 3], [4, 5, 6], [0, 7, 8]], True),
+        ("position 8 can move right", [[1, 2, 3], [4, 5, 6], [7, 0, 8]], True),
+        ("position 9 cannot move right", [[1, 2, 3], [4, 5, 6], [7, 8, 0]], False)
+    ])
+    def test_can_move_right(self, _test_name, puzzle, expected):
+        """ Tests whether the blank space can move RIGHT """
+        test_state = np.asarray(puzzle, dtype=np.int32)
+        self._puzzle_state = PuzzleState(test_state, 0, None)
+        actual = self._puzzle_state.can_move('right')
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        (
+            "blank moves up and swaps with 2",
+            'up',
+            [[1, 2, 3], [4, 0, 5], [6, 7, 8]],
+            [[1, 0, 3], [4, 2, 5], [6, 7, 8]]
+        ), (
+            "blank moves down and swaps with 7",
+            'down',
+            [[1, 2, 3], [4, 0, 5], [6, 7, 8]],
+            [[1, 2, 3], [4, 7, 5], [6, 0, 8]]
+        ), (
+            "blank moves left and swaps with 4",
+            'left',
+            [[1, 2, 3], [4, 0, 5], [6, 7, 8]],
+            [[1, 2, 3], [0, 4, 5], [6, 7, 8]]
+        ), (
+            "blank moves right and swaps with 5",
+            'right',
+            [[1, 2, 3], [4, 0, 5], [6, 7, 8]],
+            [[1, 2, 3], [4, 5, 0], [6, 7, 8]]
+        )
+    ])
+    def test_gen_next_state(self, _test_name, direction, current_puzzle, desired_puzzle):
+        """ Test that the next state is generated when given a direction """
+        test_state = np.asarray(current_puzzle, dtype=np.int32)
+        self._puzzle_state = PuzzleState(test_state, 0, None)
+        self._puzzle_state = self._puzzle_state.gen_next_state(direction)
+
+        actual_puzzle = self._puzzle_state.puzzle
+        numpy.testing.assert_allclose(desired_puzzle, actual_puzzle)
+
+        actual_predecessor_action = self._puzzle_state.action_from_predecessor
+        self.assertEqual(direction, actual_predecessor_action)
 
     def test_show_path(self):
-        """ TODO """
+        """ Test the output of the main process at its END state. """
+        # Create the path and capture the output.
+        captured_output = io.StringIO()  # Create StringIO object
+        sys.stdout = captured_output     # and redirect stdout.
+        self._puzzle_state = self._puzzle_state.gen_next_state('right')
+        self._puzzle_state = self._puzzle_state.gen_next_state('up')
+        self._puzzle_state = self._puzzle_state.gen_next_state('left')
+        self._puzzle_state = self._puzzle_state.gen_next_state('up')
+        self._puzzle_state.show_path()   # Call the function.
 
-    def test_can_move(self):
-        """ TODO """
+        expected_output = """START
+[[3 1 2]
+ [4 7 5]
+ [0 6 8]]
+Move 1 ACTION: right
+[[3 1 2]
+ [4 7 5]
+ [6 0 8]]
+Move 2 ACTION: up
+[[3 1 2]
+ [4 0 5]
+ [6 7 8]]
+Move 3 ACTION: left
+[[3 1 2]
+ [0 4 5]
+ [6 7 8]]
+Move 4 ACTION: up
+[[0 1 2]
+ [3 4 5]
+ [6 7 8]]"""
+        self.assertIn(expected_output, captured_output.getvalue())
 
-    def test_gen_next_state(self):
-        """ TODO """
 
 if __name__ == '__main__':
     unittest.main()
